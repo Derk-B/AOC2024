6360bb134df617c94eca5f49079b7410fb28a45c
Derk <74244634+Derk-B@users.noreply.github.com>
2024-12-11 12:41:43 +0100

    day2 part 2

--- day5/main.py
+++ day5/main.py
@@ -14,9 +14,6 @@ rules = [[int(a), int(b)] for [a, b] in [rule.split('|') for rule in rules]]
 key_set = set([k for [k, v] in rules])
 val_set = set([v for [k, v] in rules])
 
-for v in val_set:
-    has_seen_map[v] = False
-
 for k in key_set:
     rule_map[k] = []
 
@@ -24,25 +21,55 @@ for [k,v] in rules:
     rule_map[k].append(v)
 
 def calculate_update(update):
-    for number in update:
+    # Reset has_seen_map
+    for k in has_seen_map:
+        has_seen_map[k] = None
+
+    for i, number in enumerate(update):
         order_values = rule_map.get(number, [])
-        if order_values == None: 
-            order_values = []
         
         for ov in order_values:
-            if has_seen_map.get(ov, False) == True:
+            if has_seen_map.get(ov, None) != None:
                 return 0
         
-        has_seen_map[number] = True
+        has_seen_map[number] = i
     
     return update[int(len(update)/2)]
 
+def check_position(order_values, numbers, number, i):
+    for ov in order_values:
+        conflict_index = has_seen_map.get(ov, None)
+        if conflict_index != None:
+            numbers[i] = ov
+            numbers[conflict_index] = number
+            return numbers
 
-result = 0
-for update in updates:
-    result += calculate_update([int(x) for x in update.split(',')])
+def part2(update):
+    update_numbers = update
     # Reset has_seen_map
     for k in has_seen_map:
-        has_seen_map[k] = False
+        has_seen_map[k] = None
+    
+    for i, number in enumerate(update_numbers):
+        order_values = rule_map.get(number, [])
+        new_array = check_position(order_values, update_numbers, number, i)
+        if new_array != None:
+            return part2(new_array)
+            
+        has_seen_map[number] = i
+
+    return update_numbers[int(len(update)/2)]
+
+result = 0
+result2 = 0
+for update in updates:
+    numbers = [int(x) for x in update.split(',')]
+    ordered_result = calculate_update(numbers)
+    if ordered_result == 0:
+        result2 += part2(numbers)
+    else:
+        result += ordered_result
+
 
 print("res1 " , result)
+print('res2 ', result2)
